126fdb0ec6a58590ddb1ec7220f087d2c7e3c0c2
komalshinde3198-coder <komalshinde3198@gmail.com>
2026-04-17 22:00:23 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state-123"
+    bucket = "young-minds-app-project-terraform-state-1235"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
